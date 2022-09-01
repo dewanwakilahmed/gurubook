4d155b370d7eee335dba7e2be349087d79db460f
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-01 20:10:50 +0600

    Installed React Router Dom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+// CSS
 import "./index.css";
 
+// Components
 import App from "./App";
 
+// Misc
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
